c958bfa1bfa80772abd10d1795c0975b2a6d1e2c
joelj11projects <joeljoshy11@gmail.com>
2018-08-07 17:11:23 +0100

    initial

---
+++ src/account_application/App.java
@@ -0,0 +1,11 @@
+package account_application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world to the standard out");
+		
+	}
+
+}
