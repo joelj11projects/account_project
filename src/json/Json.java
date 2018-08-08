5241ebfadafbf97365054c4311b273a3fec0a501
joelj11projects <joeljoshy11@gmail.com>
2018-08-08 17:04:02 +0100

    update

---
+++ src/json/Json.java
@@ -0,0 +1,20 @@
+package json;
+
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import management_system.Account;
+
+public class Json {
+
+	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
+	public String jsonConverter(Map<String, Account> jsonList) {
+		
+		return gson.toJson(jsonList);
+	}
+		
+	
+}
